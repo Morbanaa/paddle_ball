4985bf553c0695c29320cf7b3d36e965907e0e91
Morbanaa <theodorerodd@gmail.com>
2026-04-20 20:03:48 -0500

    Created nested for loops to generate the world and store and return it in a list called world_map

--- classes.py
+++ classes.py
@@ -1,8 +1,21 @@
 from abc import ABC, abstractmethod
 
 class GameControl():
-    def world_gen():
-        pass
+    
+    world_map = []
+    def __init__(self,game_height,game_width):
+        self.game_height = game_height
+        self.game_width = game_width
+
+    def world_gen(self,world_map):      
+        for y in range(self.game_height):
+            row = []
+            for x in range(self.game_width):
+                if y == 0 or y == self.game_height -1 or x == 0 or x == self.game_width -1:
+                    row.append("@")
+            world_map.append(row)
+        return world_map
+
 
     def render_world():
         pass
@@ -12,6 +25,8 @@ class GameObject():
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
+
+    # All sub classes must have these methods...
     @abstractmethod
     def update_position():
         pass
