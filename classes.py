c42d06c969cd774bc75afb7860b89aa0fb15ebd6
Morbanaa <theodorerodd@gmail.com>
2026-04-20 19:30:02 -0500

    Added class outlines

--- classes.py
+++ classes.py
@@ -1,1 +1,27 @@
-ECHO is on.
+from abc import ABC, abstractmethod
+
+class GameControl():
+    pass
+
+class GameObject():
+    def __init__(self,ypos,xpos):
+        self.ypos = ypos
+        self.xpos = xpos
+
+class Ball(GameObject):
+    def collison():
+        pass
+
+class Charactor(ABC,GameObject):
+    @abstractmethod
+    def update_position():
+        pass
+
+class PlayerOne(Charactor):
+    def update_position():
+        pass
+
+class PlayerTwo(Charactor):
+    def update_position():
+        pass
+    
