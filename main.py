cd8ed1acd6668da4c80a5566c6b7e21524f74f18
Morbanaa <theodorerodd@gmail.com>
2026-04-21 15:49:48 -0500

    created main game loop drawing the map each frame and moving the cursor to the top of the screen

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from classes import GameControl
 
 def main():
     game_control = GameControl(25,90)
-
+    
     game_control.world_gen()
-    game_control.render_world()
+    while True:
+        game_control.clear_move_cursor()
+        game_control.render_world()
 
 #Program Entry Point
 if __name__ == "__main__":
